9be17ee925062c14b2e0f5c289d3049f56eccc6b
hoho2017 <1063907253@qq.com>
2020-09-11 18:02:03 +0800

    配置提交

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:promise/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'prettier/unicorn',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+      },
+    },
+  },
+  plugins: ['react', 'unicorn', 'promise', '@typescript-eslint'],
+  rules: { 'linebreak-style': [0, 'error', 'windows'] },
+};
---
+++ index.ts
@@ -0,0 +1,6 @@
+const add = (a: number, b: number): number => {
+  // eslint-disable-next-line no-console
+  console.log(a + b);
+  return a + b;
+};
+export default add;
